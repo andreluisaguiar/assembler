9ea58c7ef867ae7db22b3800b4096c9684648c4f
andreluisaguiar <andre.nascimento@saude.ma.gov.br>
2026-07-03 13:21:55 -0300

    feat(parser): implementa hasMoreInstructions e advance

--- parser/parser.py
+++ parser/parser.py
@@ -7,6 +7,8 @@ class Parser:
     def __init__(self, file_path):
         self.file_path = Path(file_path)
         self.instructions = self._load_instructions()
+        self.current_index = -1
+        self.current_instruction = None
 
     def _load_instructions(self):
         instructions = []
@@ -19,3 +21,15 @@ class Parser:
                     instructions.append("".join(instruction.split()))
 
         return instructions
+
+    def hasMoreInstructions(self):
+        return self.current_index + 1 < len(self.instructions)
+
+    def advance(self):
+        if not self.hasMoreInstructions():
+            self.current_instruction = None
+            return None
+
+        self.current_index += 1
+        self.current_instruction = self.instructions[self.current_index]
+        return self.current_instruction
