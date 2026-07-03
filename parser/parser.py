a3f838aef8e1d7b4658e3659afb09c0f286e6f53
andreluisaguiar <andre.nascimento@saude.ma.gov.br>
2026-07-03 13:23:31 -0300

    feat(parser): implementa symbol(), dest(), comp() e jump()

--- parser/parser.py
+++ parser/parser.py
@@ -56,6 +56,54 @@ class Parser:
 
         return self.C_INSTRUCTION
 
+    def symbol(self):
+        instruction_type = self.instructionType()
+
+        if instruction_type == self.A_INSTRUCTION:
+            return self.current_instruction[1:]
+
+        if instruction_type == self.L_INSTRUCTION:
+            return self.current_instruction[1:-1]
+
+        raise ValueError("symbol() is valid only for A or label instructions")
+
+    def dest(self):
+        self._ensure_c_instruction()
+
+        if "=" not in self.current_instruction:
+            return None
+
+        return self.current_instruction.split("=", 1)[0]
+
+    def comp(self):
+        self._ensure_c_instruction()
+
+        instruction = self.current_instruction
+
+        if "=" in instruction:
+            instruction = instruction.split("=", 1)[1]
+
+        if ";" in instruction:
+            instruction = instruction.split(";", 1)[0]
+
+        return instruction
+
+    def jump(self):
+        self._ensure_c_instruction()
+
+        if ";" not in self.current_instruction:
+            return None
+
+        return self.current_instruction.split(";", 1)[1]
+
+    def reset(self):
+        self.current_index = -1
+        self.current_instruction = None
+
     def _ensure_current_instruction(self):
         if self.current_instruction is None:
             raise ValueError("No current instruction. Call advance() first.")
+
+    def _ensure_c_instruction(self):
+        if self.instructionType() != self.C_INSTRUCTION:
+            raise ValueError("This method is valid only for C instructions")
